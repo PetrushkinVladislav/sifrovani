623d0233356fd8b38c542009dc8024bdbc8e6360
PetrushkinVladislav <62994591+PetrushkinVladislav@users.noreply.github.com>
2021-02-17 18:15:04 +0700

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Circle extends Shape {
+    public void drow() {
+        System.out.println("Круг");
+    }
+}
---
+++ Square.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Square extends Shape {
+    public void drow() {
+        System.out.println("Квадрат");
+    }
+}
